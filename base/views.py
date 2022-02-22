c73acde4d8008f84ed5f36fe72ceb55030b65b6a
Shihongji <shihongji@github.com>
2022-02-22 22:17:53 +0800

    finish but lxwg-wenkai font failed

--- base/views.py
+++ base/views.py
@@ -64,6 +64,7 @@ def registerPage(request):
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms_sum = Room.objects.all().count()
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
@@ -71,10 +72,10 @@ def home(request):
         Q(host__username__icontains=q)
     )
     room_count = rooms.count()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     context = {'rooms': rooms, 'topics': topics,
-               'room_count': room_count, 'room_messages': room_messages}
+               'room_count': room_count, 'room_messages': room_messages, 'rooms_sum':rooms_sum}
     return render(request, 'base/home.html', context)
 
 
@@ -186,3 +187,15 @@ def updateUser(request):
             form.save()
             return redirect('user-profile', pk=user.id)
     return render(request, 'base/update_user.html', {'form':form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    
+    rooms_sum = Room.objects.all().count()
+    
+    return render(request, 'base/topics.html', {'topics':topics, 'rooms_sum':rooms_sum})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages':room_messages})
